7e8d35277fdaabba911ab5b31755384144c9fef7
pourbaixe <pourbaixe@educ.college-montreal.qc.ca>
2022-09-29 10:39:08 -0400

    début du TP2

--- TP2.py
+++ TP2.py
@@ -6,7 +6,7 @@ TP2- jeu de devinettes
 from random import *
 
 chiffre_choisit = randint(0,100)
-nb_essai = 0
+#nb_essai = 0 ?
 
 while;
 print("J'ai choisi un nombre entre 0 et 100. À vous de le deviner...")
