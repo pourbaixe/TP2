d8d60b6cded1be5a91dfd5d329bf50134c771905
pourbaixe <pourbaixe@educ.college-montreal.qc.ca>
2022-09-29 10:36:59 -0400

    début du TP2

---
+++ TP2.py
@@ -0,0 +1,14 @@
+"""
+Évangéline Pourbaix
+Gr:402
+TP2- jeu de devinettes
+"""
+from random import *
+
+chiffre_choisit = randint(0,100)
+nb_essai = 0
+
+while;
+print("J'ai choisi un nombre entre 0 et 100. À vous de le deviner...")
+
+
