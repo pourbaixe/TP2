9a0ddd902b5b0ca51915b8781082e565e047c3dc
pourbaixe <pourbaixe@educ.college-montreal.qc.ca>
2022-09-30 09:16:32 -0400

    TP2 début

--- TP2.py
+++ TP2.py
@@ -5,10 +5,18 @@ TP2- jeu de devinettes
 """
 from random import *
 
-chiffre_choisit = randint(0,100)
-nb_essai = 0
+chiffre_choisit = randint(0, 5)
 
-while;
-print("J'ai choisi un nombre entre 0 et 100. À vous de le deviner...")
-
-
+boucle_jeu = True
+while boucle_jeu:
+    print("J'ai choisi un nombre entre 0 et 5. À vous de le deviner")
+    if numero_joue == chiffre_choisit:
+        fin_jeu(str(input("C'est le bon numéro, voulez-vous rejouer (oui/non)?")
+    if fin_jeu == "oui":
+        boucle_jeu = True
+    else fin_jeu == "non":
+        boucle_jeu = False
+    if numero_joue < chiffre_choisit:
+        print("Le chiffre est plus petit, réessayez")
+    elif numero_joue > chiffre_choisit:
+        print("Le chiffre est plus grand, réessayez")
