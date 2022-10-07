aa30e0bb30d0dca3d19c6af60f443a9a036c9a44
pourbaixe <pourbaixe@educ.college-montreal.qc.ca>
2022-10-07 16:01:57 -0400

    TP2 début

--- TP2.py
+++ TP2.py
@@ -9,14 +9,14 @@ chiffre_choisit = randint(0, 5)
 
 boucle_jeu = True
 while boucle_jeu:
-    print("J'ai choisi un nombre entre 0 et 5. À vous de le deviner")
+    numero_joue = int(input("J'ai choisi un nombre entre 0 et 5. À vous de le deviner"))
     if numero_joue == chiffre_choisit:
-        fin_jeu(str(input("C'est le bon numéro, voulez-vous rejouer (oui/non)?")
-    if fin_jeu == "oui":
-        boucle_jeu = True
-    else fin_jeu == "non":
-        boucle_jeu = False
-    if numero_joue < chiffre_choisit:
+        fin_jeu = str(input("C'est le bon numéro, voulez-vous rejouer (oui/non)?"))
+        if fin_jeu == "oui":
+            boucle_jeu = True
+        elif fin_jeu == "non":
+            boucle_jeu = False
+    if numero_joue > chiffre_choisit:
         print("Le chiffre est plus petit, réessayez")
-    elif numero_joue > chiffre_choisit:
+    elif numero_joue < chiffre_choisit:
         print("Le chiffre est plus grand, réessayez")
